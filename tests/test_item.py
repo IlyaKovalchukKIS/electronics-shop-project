c9a36b72234c100b0204ebe3e47573b6941d83c9
ilya <mrlivedance@vk.com>
2023-05-15 16:56:55 +0300

    Написаны тесты для items.py класса Items

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_items():
+    item1 = Item("Смартфон", 10_000, 20)
+    assert isinstance(item1, Item)
+    assert item1.calculate_total_price() == 200_000
+
+    Item.pay_rate = 0.85
+    item1.apply_discount()
+    assert item1.price == 8_500
