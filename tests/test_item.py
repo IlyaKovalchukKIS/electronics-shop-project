cf2950b1237f5a20905416a16c5f5ae65b1d4fba
ilya <mrlivedance@vk.com>
2023-05-17 18:21:25 +0300

    Тесты разного функционала разнесены по отдельным функциям

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,11 +2,18 @@
 from src.item import Item
 
 
-def test_items():
+def test_items_total_price():
     item1 = Item("Смартфон", 10_000, 20)
-    assert isinstance(item1, Item)
     assert item1.calculate_total_price() == 200_000
 
+
+def test_items_init():
+    item1 = Item("Смартфон", 10_000, 20)
+    assert isinstance(item1, Item)
+
+
+def test_items_discount():
+    item1 = Item("Смартфон", 10_000, 20)
     Item.pay_rate = 0.85
     item1.apply_discount()
     assert item1.price == 8_500
